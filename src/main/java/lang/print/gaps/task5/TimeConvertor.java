33b4e4083502c0f3f6cc9d5089bbfac5f8f57b0e
JoseOlguin <movistar.8331560462@gmail.com>
2024-07-21 22:28:35 -0600

    Commit for: stage0-module3-task5

--- src/main/java/lang/print/gaps/task5/TimeConvertor.java
+++ src/main/java/lang/print/gaps/task5/TimeConvertor.java
@@ -2,5 +2,7 @@ package lang.print.gaps.task5;
 
 public class TimeConvertor {
     public void convert(float minutes) {
+        float seconds = minutes * 60.0f;
+        System.out.println(seconds);
     }
 }
